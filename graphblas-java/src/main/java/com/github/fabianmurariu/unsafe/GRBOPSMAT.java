c658c758038e73422b2a0cde8ae8bd090a1ec2bc
FlorentinD <florentin.doerre@tu-dresden.de>
2020-09-07 18:17:42 +0200

    Fix javadoc * escape "<" and ">" for javadoc to build * fill empty return descriptions

--- graphblas-java/src/main/java/com/github/fabianmurariu/unsafe/GRBOPSMAT.java
+++ graphblas-java/src/main/java/com/github/fabianmurariu/unsafe/GRBOPSMAT.java
@@ -9,7 +9,7 @@ public class GRBOPSMAT {
     }
 
     /**
-     * C<Mask> = accum (C, A*B)
+     * {@code C<Mask> = accum (C, A*B)}
      *
      * @param C input/output matrix for results
      * @param mask optional mask for C, unused if NULL
@@ -18,13 +18,12 @@ public class GRBOPSMAT {
      * @param A first input: matrix A
      * @param B second input: matrix B
      * @param desc descriptor for C, Mask, A, and B
-     * @return
-     *  GrB_Info status
+     * @return GrB_Info status
      */
     public static native int mxm(Buffer C, Buffer mask, Buffer accum, Buffer semiring, Buffer A, Buffer B, Buffer desc);
 
     /**
-     * C<Mask> = accum (C, u'*A)
+     * {@code C<Mask> = accum (C, u'*A)}
      *
      * @param w input/output vector for results
      * @param mask optional mask for C, unused if NULL
@@ -33,13 +32,12 @@ public class GRBOPSMAT {
      * @param u first input: vector u
      * @param A second input: matrix A
      * @param desc descriptor for C, Mask, A, and B
-     * @return
-     *  GrB_Info status
+     * @return GrB_Info status
      */
     public static native int vxm(Buffer w, Buffer mask, Buffer accum, Buffer semiring, Buffer u, Buffer A, Buffer desc);
 
     /**
-     * C<Mask> = accum (C, A*u)
+     * {@code C<Mask> = accum (C, A*u)}
      *
      * @param w input/output vector for results
      * @param mask optional mask for C, unused if NULL
@@ -48,13 +46,12 @@ public class GRBOPSMAT {
      * @param A first input: matrix A
      * @param u second input: vector u
      * @param desc descriptor for C, Mask, A, and B
-     * @return
-     *  GrB_Info status
+     * @return GrB_Info status
      */
     public static native int mxv(Buffer w, Buffer mask, Buffer accum, Buffer semiring, Buffer A, Buffer u, Buffer desc);
 
     /**
-     * C<Mask> = accum (C, A.*B)
+     * {@code C<Mask> = accum (C, A.*B)}
      *
      * @param C input/output matrix for results
      * @param mask optional mask for C, unused if NULL
@@ -63,13 +60,12 @@ public class GRBOPSMAT {
      * @param A first input: matrix A
      * @param B second input: matrix B
      * @param desc descriptor for C, Mask, A, and B
-     * @return
-     *  GrB_Info status
+     * @return GrB_Info status
      */
     public static native int elemWiseMulIntersectMonoid(Buffer C, Buffer mask, Buffer accum, Buffer monoid, Buffer A, Buffer B, Buffer desc);
 
     /**
-     * C<Mask> = accum (C, A.*B)
+     * {@code C<Mask> = accum (C, A.*B)}
      *
      * @param C input/output matrix for results
      * @param mask optional mask for C, unused if NULL
@@ -78,13 +74,12 @@ public class GRBOPSMAT {
      * @param A first input: matrix A
      * @param B second input: matrix B
      * @param desc descriptor for C, Mask, A, and B
-     * @return
-     *  GrB_Info status
+     * @return GrB_Info status
      */
     public static native int elemWiseMulIntersectBinOp(Buffer C, Buffer mask, Buffer accum, Buffer binOp, Buffer A, Buffer B, Buffer desc);
 
     /**
-     * C<Mask> = accum (C, A.+B)
+     * {@code C<Mask> = accum (C, A.+B)}
      *
      * @param C input/output matrix for results
      * @param mask optional mask for C, unused if NULL
@@ -93,13 +88,12 @@ public class GRBOPSMAT {
      * @param A first input: matrix A
      * @param B second input: matrix B
      * @param desc descriptor for C, Mask, A, and B
-     * @return
-     *  GrB_Info status
+     * @return GrB_Info status
      */
     public static native int elemWiseAddUnionMonoid(Buffer C, Buffer mask, Buffer accum, Buffer monoid, Buffer A, Buffer B, Buffer desc);
 
     /**
-     * C<Mask> = accum (C, A.+B)
+     * {@code C<Mask> = accum (C, A.+B)}
      *
      * @param C input/output matrix for results
      * @param mask optional mask for C, unused if NULL
@@ -108,13 +102,12 @@ public class GRBOPSMAT {
      * @param A first input: matrix A
      * @param B second input: matrix B
      * @param desc descriptor for C, Mask, A, and B
-     * @return
-     *  GrB_Info status
+     * @return GrB_Info status
      */
     public static native int elemWiseAddUnionBinOp(Buffer C, Buffer mask, Buffer accum, Buffer binOp, Buffer A, Buffer B, Buffer desc);
 
     /**
-     * C<Mask>(I,J) = accum (C(I,J),A)
+     * {@code C<Mask>(I,J) = accum (C(I,J),A)}
      *
      * @param C input/output matrix for results
      * @param mask optional mask for C, unused if NULL
@@ -125,12 +118,12 @@ public class GRBOPSMAT {
      * @param J column indices
      * @param nj number of column indices
      * @param desc descriptor for C, Mask, and A
-     * @return
+     * @return GrB_Info status
      */
     public static native long assign(Buffer C, Buffer mask, Buffer accum, Buffer A, long[] I, long ni, long[] J, long nj, Buffer desc);
 
     /**
-     * C(I,J)<Mask> = accum (C(I,J),A)
+     * {@code C(I,J)<Mask> = accum (C(I,J),A)}
      *
      * @param C input/output matrix for results
      * @param mask optional mask for C, unused if NULL
@@ -141,12 +134,12 @@ public class GRBOPSMAT {
      * @param J column indices
      * @param nj number of column indices
      * @param desc descriptor for C, Mask, and A
-     * @return
+     * @return GrB_Info status
      */
     public static native long subAssign(Buffer C, Buffer mask, Buffer accum, Buffer A, long[] I, long ni, long[] J, long nj, Buffer desc);
 
     /**
-     * C<Mask> = accum (C, A(I,J))
+     * {@code C<Mask> = accum (C, A(I,J))}
      *
      * @param C input/output matrix for results
      * @param mask optional mask for C, unused if NULL
@@ -157,7 +150,7 @@ public class GRBOPSMAT {
      * @param J column indices
      * @param nj number of column indices
      * @param desc descriptor for C, Mask, and A
-     * @return
+     * @return GrB_Info status
      */
     public static native long extract(Buffer C, Buffer mask, Buffer accum, Buffer A, long[] I, long ni, long[] J, long nj, Buffer desc);
 
